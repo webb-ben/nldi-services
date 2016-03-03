00f5797145a64682187b35ded2372fbe47081625
Steinich, Dave R. <drsteini@usgs.gov>
2016-03-03 12:29:46 -0600

    Bug Fixes from rename of project

--- src/main/java/gov/usgs/owi/nldi/springinit/SpringConfig.java
+++ src/main/java/gov/usgs/owi/nldi/springinit/SpringConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan(basePackages="gov.usgs.owi.navigation_pilot")
+@ComponentScan(basePackages="gov.usgs.owi.nldi")
 @EnableWebMvc
 public class SpringConfig extends WebMvcConfigurerAdapter {
 
