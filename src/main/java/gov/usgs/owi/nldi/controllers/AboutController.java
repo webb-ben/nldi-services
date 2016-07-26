bf97684bcfc79f0585b717d9216bbb6680bc2ebd
Steinich, Dave R. <drsteini@usgs.gov>
2016-07-26 10:01:27 -0500

    WQP-859

--- src/main/java/gov/usgs/owi/nldi/controllers/AboutController.java
+++ src/main/java/gov/usgs/owi/nldi/controllers/AboutController.java
@@ -1,5 +1,7 @@
 package gov.usgs.owi.nldi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import gov.usgs.owi.nldi.ApplicationVersion;
+import gov.usgs.owi.nldi.AtomReaderUtil;
 
 @Controller
 @RequestMapping(value="about")
 public class AboutController {
 
+	private String confluenceUrl;
+
+	@Autowired
+	public AboutController(@Qualifier("confluenceUrl") String confluenceUrl) {
+		this.confluenceUrl = confluenceUrl;
+	}
+
 	@GetMapping
 	public ModelAndView getIndex() {
 		ModelAndView mv = new ModelAndView("about");
 		mv.addObject("version", ApplicationVersion.getVersion());
+		mv.addObject("userGuide", AtomReaderUtil.getAtomFeedContentOnlyAsString(confluenceUrl));
 		return mv;
 	}
 
@@ -23,6 +34,7 @@ public class AboutController {
 	public ModelAndView getpage(@PathVariable("requestedPage") String requestedPage) {
 		ModelAndView mv = new ModelAndView(requestedPage);
 		mv.addObject("version", ApplicationVersion.getVersion());
+		mv.addObject("userGuide", AtomReaderUtil.getAtomFeedContentOnlyAsString(confluenceUrl));
 		return mv;
 	}
 
--- src/main/webapp/WEB-INF/views/about.jsp
+++ src/main/webapp/WEB-INF/views/about.jsp
@@ -1,5 +1,14 @@
-<head>
-</head>
-<body>
-Hello World! ${version}
-</body>
+<html>
+	<head>
+		<title>NLDI Services</title>
+	</head>
+	<body>
+		<header><h1 style="text-align:center">NLDI Services</h1></header>
+		<div class="page-content">
+			<div id="userguide-content" class="user-guide">
+				${userGuide}
+			</div>
+		</div>
+	</body>
+	<footer style="text-align:right">${version}</footer>
+</html>
