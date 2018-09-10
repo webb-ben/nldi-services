6ac3a2ddc1bafd27bd0c0b112b58afbadd7a6b15
Andrew Yan <ayan@usgs.gov>
2018-09-10 15:55:53 -0500

    fix incorrect conditional statement

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,13 +4,12 @@ set -e
 # Read environment variables from file and export them.
 file_env() {
 	while read -r line || [[ -n $line ]]; do
-		echo $line
 		export $line
 	done < "$1"
 }
 
-if [ -z $MOUNT_DIRECTORY ]; then
-	# if mount exists then export enviroment variables
+if ! [ -z $MOUNT_DIRECTORY ]; then
+	# if mount exists then export environment variables
 	for FILE in $MOUNT_DIRECTORY/*.env; do
 		file_env $FILE
 	done
