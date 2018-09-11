2154e7a81ab5a1bf8e5a30679df1c23b09621910
Andrew Yan <ayan@usgs.gov>
2018-09-11 09:18:06 -0500

    trival modification to entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,15 +3,15 @@ set -e
 
 # Read environment variables from file and export them.
 file_env() {
-	while read -r line || [[ -n $line ]]; do
-		export $line
+	while read -r line || [[ -n ${line} ]]; do
+		export ${line}
 	done < "$1"
 }
 
-if ! [ -z $MOUNT_DIRECTORY ]; then
+if  ! [ -z ${MOUNT_DIRECTORY} ]; then
 	# if mount exists then export environment variables
-	for FILE in $MOUNT_DIRECTORY/*.env; do
-		file_env $FILE
+	for FILE in ${MOUNT_DIRECTORY}/*.env; do
+		file_env ${FILE}
 	done
 fi
 
