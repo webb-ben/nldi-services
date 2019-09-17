d7aa3e4d5589cf4af890697a9cf1b9b2fa27f1f6
David Steinich <drsteini@contractor.usgs.gov>
2019-09-17 15:03:40 -0500

    CORS

--- src/main/java/gov/usgs/owi/nldi/springinit/SpringConfig.java
+++ src/main/java/gov/usgs/owi/nldi/springinit/SpringConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -28,4 +29,13 @@ public class SpringConfig implements WebMvcConfigurer {
 		return resolver;
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+			.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "OPTIONS")
+			.allowedHeaders("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+			.exposedHeaders("feature_count", "flowLine_count");
+	}
 }
