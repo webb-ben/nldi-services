02755e32f8e04ee0334c4d30f802b44dbd834d8a
Ethan Grahn <egrahn@usgs.gov>
2022-06-13 11:49:15 -0500

    Update config to utilize content negotiation

--- src/main/java/gov/usgs/owi/nldi/springinit/SpringConfig.java
+++ src/main/java/gov/usgs/owi/nldi/springinit/SpringConfig.java
@@ -1,25 +1,21 @@
 package gov.usgs.owi.nldi.springinit;
 
 
-import gov.usgs.owi.nldi.services.Parameters;
+import gov.usgs.owi.nldi.controllers.BaseController;
+import gov.usgs.owi.nldi.converters.ComidMessageConverter;
+import gov.usgs.owi.nldi.converters.ErrorMessageConverter;
+import gov.usgs.owi.nldi.converters.FeatureMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.accept.ContentNegotiationStrategy;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.converter.*;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @Configuration
+@EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
 
 	@Override
@@ -37,35 +33,24 @@ public class SpringConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.strategies(Arrays.asList(new ContentNegotiationStrategy() {
-			@Override
-			public List<MediaType> resolveMediaTypes(NativeWebRequest webRequest)
-				throws HttpMediaTypeNotAcceptableException {
+		configurer.ignoreAcceptHeader(false)
+				.favorParameter(true)
+				.parameterName("f")
+				.mediaType("jsonld", MediaType.valueOf(BaseController.MIME_TYPE_JSONLD))
+				.mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("geojson", MediaType.valueOf(BaseController.MIME_TYPE_GEOJSON))
+				.mediaType("html", MediaType.TEXT_HTML)
+				.defaultContentType(MediaType.APPLICATION_JSON);
+	}
 
-				//If the user specifies output with the format parameter, give them what they asked for.
-				Map<String, String[]> map = webRequest.getParameterMap();
-				if (map != null) {
-					String[] values = map.get(Parameters.FORMAT);
-					if (values != null) {
-						if (values[0].toLowerCase().equals("json")) {
-							return Arrays.asList(MediaType.APPLICATION_JSON);
-						} else if (values[0].toLowerCase().equals("html")) {
-							return Arrays.asList(MediaType.TEXT_HTML);
-						}
-					}
-				}
-
-				//Browsers have 'text/html' as the first element in their accept headers,
-				// so if it is the first element, the user has stumbled to this url in the
-				// browser and may not expect a json dump.
-				String accept = webRequest.getHeader(HttpHeaders.ACCEPT);
-				if (accept != null && accept.startsWith(MediaType.TEXT_HTML_VALUE)) {
-					return Arrays.asList(MediaType.TEXT_HTML);
-				} else {
-					return Arrays.asList(MediaType.APPLICATION_JSON);
-				}
-			}
-		}));
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> messageConverters) {
+		MediaType geoJson = MediaType.valueOf(BaseController.MIME_TYPE_GEOJSON);
+		MediaType jsonLd = MediaType.valueOf(BaseController.MIME_TYPE_JSONLD);
+		// we want our converters to take priority, so we add them to the top of the list
+		messageConverters.add(0, new ComidMessageConverter(geoJson, MediaType.APPLICATION_JSON));
+		messageConverters.add(0, new FeatureMessageConverter(geoJson,jsonLd,MediaType.APPLICATION_JSON));
+		messageConverters.add(0, new ErrorMessageConverter(MediaType.ALL));
 	}
 
 	@Override
@@ -74,7 +59,7 @@ public class SpringConfig implements WebMvcConfigurer {
 			.addMapping("/**")
 			.allowedOrigins("*")
 			.allowedMethods("GET", "OPTIONS")
-			.allowedHeaders("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers")
-			.exposedHeaders("feature_count", "flowLine_count");
+			.allowedHeaders("Origin", "Accept", "X-Requested-With", "Content-Type",
+					"Access-Control-Request-Method", "Access-Control-Request-Headers");
 	}
 }
