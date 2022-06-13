7da909216726af2595f9b78cb837175ea85175e1
Ethan Grahn <egrahn@usgs.gov>
2022-06-13 11:51:13 -0500

    Return Error class in exception handler

--- src/main/java/gov/usgs/owi/nldi/exceptions/GlobalDefaultExceptionHandler.java
+++ src/main/java/gov/usgs/owi/nldi/exceptions/GlobalDefaultExceptionHandler.java
@@ -4,21 +4,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalDefaultExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalDefaultExceptionHandler {
   private static final Logger LOG = LoggerFactory.getLogger(GlobalDefaultExceptionHandler.class);
 
   @ExceptionHandler({
@@ -28,23 +26,36 @@ public class GlobalDefaultExceptionHandler extends ResponseEntityExceptionHandle
   })
   @ResponseStatus(value = HttpStatus.NOT_FOUND)
   @ResponseBody
-  public String handleNotFoundException(Exception exception) {
-    return exception.getLocalizedMessage();
+  public Error handleNotFoundException(Exception exception) {
+    return new Error(exception.getLocalizedMessage());
   }
 
-  @ExceptionHandler({ConstraintViolationException.class, ResponseStatusException.class})
+  @ExceptionHandler({
+          ConstraintViolationException.class,
+          ResponseStatusException.class,
+          MissingServletRequestParameterException.class
+  })
   @ResponseStatus(value = HttpStatus.BAD_REQUEST)
   @ResponseBody
-  public String handleBadRequestException(Exception exception) {
-    return exception.getLocalizedMessage();
+  public Error handleBadRequestException(Exception exception) {
+    return new Error(exception.getLocalizedMessage());
   }
 
-  @Override
-  public ResponseEntity<Object> handleHttpMessageNotReadable(
-      HttpMessageNotReadableException exception,
-      HttpHeaders headers,
-      HttpStatus status,
-      WebRequest request) {
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  @ResponseBody
+  public Error handleMethodArgumentTypeMistmatchException(MethodArgumentTypeMismatchException exception) {
+    // simplifies the message and avoids listing the nested exceptions
+    String finalMessage = exception.getMessage().split("IllegalArgumentException: ")[1];
+    return new Error(finalMessage);
+  }
+
+  @ExceptionHandler({
+          HttpMessageNotReadableException.class
+  })
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  @ResponseBody
+  public Error handleBadRequestTrimmedException(Exception exception) {
     String errorMessage;
     String currentMessage = exception.getLocalizedMessage();
 
@@ -56,28 +67,25 @@ public class GlobalDefaultExceptionHandler extends ResponseEntityExceptionHandle
       errorMessage = currentMessage.replaceAll("([a-zA-Z]+\\.)+", "");
     }
 
-    Object entityBody = errorMessage;
-    return new ResponseEntity(entityBody, headers, HttpStatus.BAD_REQUEST);
+    return new Error(errorMessage);
   }
 
-  @Override
-  protected ResponseEntity<Object> handleMissingServletRequestParameter(
-      MissingServletRequestParameterException ex,
-      HttpHeaders headers,
-      HttpStatus status,
-      WebRequest request) {
-    return new ResponseEntity<>(ex.getLocalizedMessage(), headers, HttpStatus.BAD_REQUEST);
+  @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+  @ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
+  @ResponseBody
+  public Error handleHttpMediaTypeNotAcceptableExeption(HttpMediaTypeNotAcceptableException exception) {
+    return new Error(exception.getMessage());
   }
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
   @ResponseBody
-  public String handleUncaughtException(Exception exception, HttpServletResponse response) {
+  public Error handleUncaughtException(Exception exception, HttpServletResponse response) {
     int hashValue = response.hashCode();
     // Note: we are giving the user a generic message.
     // Server logs can be used to troubleshoot problems.
     String msgText = "Something bad happened. Contact us with Reference Number: " + hashValue;
     LOG.error(msgText, exception);
-    return msgText;
+    return new Error(msgText);
   }
 }
