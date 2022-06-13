3d5d1be470426dfecfdd7b69f62a5e1c1d06d851
Ethan Grahn <egrahn@usgs.gov>
2022-06-13 11:50:10 -0500

    Create model classes for return types

---
+++ src/main/java/gov/usgs/owi/nldi/model/DataSource.java
@@ -0,0 +1,42 @@
+package gov.usgs.owi.nldi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DataSource {
+    @JsonProperty("source")
+    private String source;
+    @JsonProperty("sourceName")
+    private String name;
+    @JsonProperty("features")
+    private String uri;
+
+    public DataSource(String source, String name, String uri) {
+        this.source = source;
+        this.name = name;
+        this.uri = uri;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
